b2fd4c483820481570072c8c8df06dbec42795e9
aaakash06 <akashbagale182@gmail.com>
2024-09-22 23:36:41 +0545

    webhook 1

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -12,7 +12,9 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   console.log("========webhook hit===========");
   // const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET =
+    process.env.WEBHOOK_SECRET || "whsec_Qg5EcGbXCK6FsU1L+04R0t70DyRDZue8";
+  console.log("the webhook secret is: ", WEBHOOK_SECRET);
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
