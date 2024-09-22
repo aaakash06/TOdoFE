d1e154106b4d775da8cc19ea2ac1030050a99779
aaakash06 <akashbagale182@gmail.com>
2024-09-22 18:01:34 +0545

    clerk middleware and db added

---
+++ src/db/actions.db.ts
@@ -0,0 +1,60 @@
+import { connectToDB } from "./connect.db";
+import { User } from "./models.db";
+
+interface CreateUserClerkType {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  picture: string;
+}
+export async function createUserByClerk(user: CreateUserClerkType) {
+  try {
+    await connectToDB();
+
+    console.log(user);
+
+    const mongoUser = await User.create(user);
+    return mongoUser;
+  } catch (err) {
+    console.log("couldn't create user in the database with clerkId");
+    console.log(err);
+  }
+}
+
+export async function updateUserByClerk(
+  id: string,
+  toUpdate: {
+    name: string;
+    username: string;
+    email: string;
+    picture: string;
+  }
+) {
+  try {
+    await connectToDB();
+
+    const mongoUser = await User.findOneAndUpdate({ clerkId: id }, toUpdate, {
+      new: true,
+    });
+    return mongoUser;
+  } catch (err) {
+    console.log("couldn't create user in the database with clerkId");
+    console.log(err);
+  }
+}
+
+export const deleteUserByClerkId = async (id: string) => {
+  try {
+    connectToDB();
+    const user = await User.findOneAndDelete({ clerkId: id });
+    if (!user) {
+      console.log("no user found to delete in db");
+      return "no user found to delete in db";
+    }
+    return user;
+  } catch (err) {
+    console.log("error occured during fetching user and deleting by id ");
+    console.log(err);
+  }
+};
---
+++ src/db/models.db.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, Document } from "mongoose";
+export type ObjectIdType = mongoose.Schema.Types.ObjectId;
+
+export interface IUser extends mongoose.Document {
+  name: string;
+  username?: string;
+  email: string;
+  emailVerified?: Date;
+  password?: string;
+  bio?: string;
+  picture?: string;
+  portfolioWebSite?: string;
+  saved?: ObjectIdType[];
+  posts?: ObjectIdType[];
+  comments?: ObjectIdType[];
+  joinAt: Date;
+  // account and session
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  emailVerified: { type: Date },
+  password: { type: String },
+  bio: { type: String },
+  picture: { type: String },
+  portfolioWebSite: { type: String },
+  saved: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+  joinAt: { type: Date, default: Date.now },
+});
+
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
