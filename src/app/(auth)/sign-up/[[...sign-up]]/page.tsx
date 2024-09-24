d56fa9ef08866a835e220c2e7c299eb10cf59f29
aaakash06 <akashbagale182@gmail.com>
2024-09-24 23:13:51 +0545

    clerk after sign up remove

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <section className="w-screen h-screen bg-light-700 flexx pt-40">
-      <SignUp forceRedirectUrl={"/role"} />
+      <SignUp />
     </section>
   );
 }
